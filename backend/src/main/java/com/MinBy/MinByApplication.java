df420c19b563fc1ba899a86801f1a642a9d30c00
JennyWinther <103136393+JennyWinther@users.noreply.github.com>
2025-02-06 17:35:17 +0100

    Satt opp backend Springboot

---
+++ backend/src/main/java/com/MinBy/MinByApplication.java
@@ -0,0 +1,13 @@
+package com.MinBy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinByApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinByApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/MinBy/MinByApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MinBy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinByApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
