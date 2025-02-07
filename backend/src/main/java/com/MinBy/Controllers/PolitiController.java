6e793c1b8b4b0f1cc4226f4352558db5ef8fcfab
JennyWinther <103136393+JennyWinther@users.noreply.github.com>
2025-02-07 07:24:55 +0100

    Fikset wrapper class og fikset CORS til frontend. Innhenting av info fra API funker!!

--- backend/src/main/java/com/MinBy/Controllers/PolitiController.java
+++ backend/src/main/java/com/MinBy/Controllers/PolitiController.java
@@ -1,20 +1,27 @@
 package com.MinBy.Controllers;
 import com.MinBy.Entiteter.Melding;
+import com.MinBy.Entiteter.MeldingWrapper;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 
 @RestController
+@RequestMapping("/politiloggen")
+@CrossOrigin(origins = "http://localhost:5173")
 public class PolitiController {
 
     private String basePolitiUrl = "https://api.politiet.no/politiloggen/v1/"; //messages?SortBy=Date
 
-    @GetMapping(value = "hentAlleMeldinger")
-    private Melding HentAlleMeldinger(){
+    @GetMapping(value = "/hentAlle")
+    private List<Melding> HentAlleMeldinger(){
         RestTemplate restTemplate = new RestTemplate();
-        Melding resultat = restTemplate.getForObject(basePolitiUrl + "messages?SortBy=Date", Melding.class);
-        return resultat;
+        MeldingWrapper resultat = restTemplate.getForObject(basePolitiUrl + "messages?SortBy=Date", MeldingWrapper.class);
+        return resultat != null ? resultat.getData() : List.of();
     }
 
 
--- backend/src/main/java/com/MinBy/Entiteter/MeldingWrapper.java
+++ backend/src/main/java/com/MinBy/Entiteter/MeldingWrapper.java
@@ -1,4 +1,15 @@
 package com.MinBy.Entiteter;
 
+import java.util.List;
+
 public class MeldingWrapper {
+    private List<Melding> data;
+
+    public List<Melding> getData() {
+        return data;
+    }
+
+    public void setData(List<Melding> data) {
+        this.data = data;
+    }
 }
