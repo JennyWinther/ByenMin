fd0f3a744139974cc39ea2650171266cf7d308ad
JennyWinther <103136393+JennyWinther@users.noreply.github.com>
2025-04-08 12:41:24 +0200

    Fiksa routes til registrering, profil og vær-siden samt flere kommentarer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,9 +7,12 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MessagePage from './components/meldinger/MessagePage.js';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import LoginPage from './components/navbar/LoginPage.js';
-import Været from './components/vær/Vær.js';
+import LoginPage from './components/bruker/LoginPage.js';
+import Vær from './components/vær/Vær.js';
+import RegisterUserPage from './components/bruker/RegisterUserPage.js';
+import ProfilSide from './components/bruker/ProfilSide.js';
 
+// Routes for applikasjonen.
 
 const router = createBrowserRouter([
   {
@@ -20,16 +23,28 @@ const router = createBrowserRouter([
     path: "/politiloggen",
     element: <MessagePage />,
   },
-  // {
-  //   path: "/vaeret",
-  //   element: <Været />,
-  // },
+  {
+    path: "/vaeret",
+    element: <Vær />,
+  },
   {
     path: "/login",
     element: <LoginPage />,
+  },
+  {
+    path: "/registrer",
+    element: <RegisterUserPage />,
+  },
+  {
+    path: "/profil",
+    element: <ProfilSide />,
   }
 ])
 
+// Her starter applikasjonen og setter opp router og styling.
+// StyledEngineProvider brukes for å sette opp MUI-stilene.
+// RouterProvider brukes for å sette opp routing i applikasjonen.
+// LocalizationProvider og AdapterDayjs brukes for å sette opp datoformatet i applikasjonen.
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StyledEngineProvider injectFirst>
